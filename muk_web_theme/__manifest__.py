f563f4576377a360994ab1b4ae2f070d4290d6c6
mibab.heli <mibab@heliconia.in>
2024-11-22 15:14:30 +0530

    ADOO-2080: [FIX] muk_web_theme: Fix error while first time opening the application in chrome browser.

--- muk_web_theme/__manifest__.py
+++ muk_web_theme/__manifest__.py
@@ -12,7 +12,7 @@
 {
     'name': 'MuK Backend Theme', 
     'summary': 'Odoo Community Backend Theme',
-    'version': '16.0.1.0.7',
+    'version': '16.0.1.0.8',
     'category': 'Themes/Backend', 
     'license': 'LGPL-3', 
     'author': 'MuK IT',
